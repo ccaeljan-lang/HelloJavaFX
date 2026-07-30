d7680b03ffb7e309726ea8be53c5cec0a86634c3
caeljandcristobal <ccaeljan@gmail.com>
2026-07-30 13:27:04 +0800

    Called the fxml file to the FlagAppWithSceneBuilder class

--- src/main/java/ph/edu/dlsu/lbycpob/hellojavafx/FlagAppWithSceneBuilder.java
+++ src/main/java/ph/edu/dlsu/lbycpob/hellojavafx/FlagAppWithSceneBuilder.java
@@ -1,4 +1,30 @@
 package ph.edu.dlsu.lbycpob.hellojavafx;
 
-public class FlagAppWithSceneBuilder {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import static javafx.application.Application.launch;
+
+public class FlagAppWithSceneBuilder extends Application {
+    @Override
+    public void start (Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("flag.fxml"));
+
+        Parent root = loader.load();
+
+        // Create the scene
+        Scene scene = new Scene(root, 1200, 638);
+
+        // Set up the window
+        stage.setTitle("My JavaFX Image");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
