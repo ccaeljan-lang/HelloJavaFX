5a4ceafb5e82a8e9bb58c5d084865e7859c7b85d
de-rekk <painganshawnderek@gmail.com>
2026-07-30 11:54:57 +0800

    Implemented MinimalistHelloApp functionalities

--- src/main/java/ph/edu/dlsu/lbycpob/hellojavafx/MinimalistHelloApp.java
+++ src/main/java/ph/edu/dlsu/lbycpob/hellojavafx/MinimalistHelloApp.java
@@ -1,4 +1,19 @@
 package ph.edu.dlsu.lbycpob.hellojavafx;
 
-public class MinimalistHelloApp {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+
+public class MinimalistHelloApp extends Application {
+    @Override
+    public void start(Stage window){
+        Label label = new Label("   \t\t   Hello LBYCPOB OOP! by Group 7 - Cristobal, Paingan, Villaluz   \t\t   ");
+        window.setTitle("HelloApplication by Group 7 - Cristobal, Paingan, Villaluz");
+        window.setScene(new Scene(label, 470, 240));
+        window.show();
+    }
+    static void main() {
+        launch();
+    }
 }
